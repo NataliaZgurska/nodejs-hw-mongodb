edff92d5a0cd1c1a3dbd88dd1eb9de573489534a
NataliaZgurska <143435353+NataliaZgurska@users.noreply.github.com>
2024-05-28 13:43:01 +0300

    create

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+import { ENV_VARS } from './constants/index.js';
+
+export const setupServer = () => {
+  const app = express();
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(cors());
+
+  app.use((req, res) => {
+    res.status(404).send('Not found');
+  });
+
+  const PORT = env(ENV_VARS.PORT, 3000);
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+  });
+};
